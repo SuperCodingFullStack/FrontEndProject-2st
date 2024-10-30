57f62bfa082a462dd5c257240f58ff51be2b17b4
hhaemin <ahrgoals@naver.com>
2024-10-30 18:57:49 +0900

    ✨feat.라우터 사용

---
+++ src/components/Wrapper/AllWrap.js
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "../../pages/Login/Login";
+import SignUp from "../../pages/Singup/SignUp";
+
+const Wrapping = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+  height: 100vh;
+  background-color: rgb(26, 27, 31);
+  overflow: hidden;
+`;
+
+const AllWrap = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/login"
+          element={
+            <Wrapping>
+              <Login />
+            </Wrapping>
+          }
+        />
+
+        <Route
+          path="/signup"
+          element={
+            <Wrapping>
+              <SignUp />
+            </Wrapping>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AllWrap;
