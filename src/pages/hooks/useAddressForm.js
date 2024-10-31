46ba1a99af45fa7def0b6946e6d468083e0a2991
hhaemin <ahrgoals@naver.com>
2024-10-31 13:18:35 +0900

    ✨feat. 주소 입력 CustomHook 생성

---
+++ src/pages/hooks/useAddressForm.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+const useAddressForm = () => {
+  const [postcode, setPostcode] = useState("");
+  const [address, setAddress] = useState("");
+  const [detailedAddress, setDetailedAddress] = useState("");
+
+  // 주소 검색을 위한 함수 (Daum 우편번호 API)
+  const handlePostcodeSearch = () => {
+    new window.daum.Postcode({
+      oncomplete: function (data) {
+        // 선택한 주소 데이터를 받아와서 필요한 정보를 업데이트
+        setPostcode(data.zonecode); // 우편번호
+        setAddress(data.address); //기본 주소
+      },
+    }).open();
+  };
+
+  return {
+    postcode,
+    address,
+    detailedAddress,
+    handlePostcodeSearch,
+    setDetailedAddress,
+  };
+};
+
+export default useAddressForm;
