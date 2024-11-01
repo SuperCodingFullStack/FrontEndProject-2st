73aa50be1253b5dda6454735fc95d7ca0e6543f2
haruka <kansaki135@gmail.com>
2024-11-01 11:28:37 +0900

    테스트

--- src/pages/Details/DetailBody.js
+++ src/pages/Details/DetailBody.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import styled from 'styled-components';
-import ImageSlider from './ImageSlider';
+import styled from "styled-components";
+import ImageSlider from "./ImageSlider";
 
 const DBody = styled.div``;
 
