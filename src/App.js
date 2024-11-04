57bb591b978bde2a2c6454ba440569b925868b85
haumbin <soung9509@gmail.com>
2024-11-04 17:46:47 +0900

    Resolved merge conflicts

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import AllWrap from "./components/Wrapper/AllWrap";
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      {/* CSS 리셋 기능 */}
-=======
       {/* CSS Reset */}
->>>>>>> be3ce8f4db460817ce18160a7ab991dd07694f95
       <Reset />
       <AllWrap />
     </>
--- src/store/store.js
+++ src/store/store.js
@@ -3,30 +3,23 @@ import popBoolSlice from "./slice/popBoolSlice";
 import activeSlice from "./slice/activeSlice";
 import footMenuSlice from "./slice/footMenuSlice";
 import dragSlice from "./slice/dragSlice";
-<<<<<<< HEAD
 import { enableMapSet } from "immer"; // Immer에서 enableMapSet 가져오기
 import cartReducer from "./slice/cartSlice";
-
-enableMapSet();
-=======
 import productSlice from "./slice/productSlice";
 import loggedSlice from "./slice/loggedSlice";
 import filterSlice from "./slice/filterSlice";
->>>>>>> be3ce8f4db460817ce18160a7ab991dd07694f95
 
+enableMapSet();
 export const store = configureStore({
   reducer: {
     popBools: popBoolSlice.reducer,
     activing: activeSlice.reducer,
     footMenu: footMenuSlice.reducer,
     drag: dragSlice.reducer,
-<<<<<<< HEAD
     cart: cartReducer,
-=======
     products: productSlice.reducer,
     log: loggedSlice.reducer,
     filter: filterSlice.reducer,
->>>>>>> be3ce8f4db460817ce18160a7ab991dd07694f95
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
