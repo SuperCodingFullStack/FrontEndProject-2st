f84d9b88ea7879b77ac38cb0045840ec054cfd58
hhaemin <ahrgoals@naver.com>
2024-11-04 11:24:49 +0900

    feat.전역상태에서 로그인/로그아웃 상태 관리

--- src/pages/hooks/useLogin.js
+++ src/pages/hooks/useLogin.js
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import api from "../../utils/api";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { login } from "../../store/slice/authSlice";
 
 const useLogin = () => {
+  const dispatch = useDispatch(); // 로그인 및 로그아웃 시 reudx 상태 업데이트
   const [error, setError] = useState(null);
   const navigate = useNavigate();
 
@@ -11,6 +14,7 @@ const useLogin = () => {
       const response = await api.post("/auth/login", { id, password });
       const token = response.data.token;
       localStorage.setItem("token", token); // JWT 토큰을 localStorage에 저장
+      dispatch(login(token));
       setError(null);
       navigate("/mypage"); // 로그인 성공 시 마이페이지로 이동
     } catch (err) {
--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,7 @@ import popBoolSlice from "./slice/popBoolSlice";
 import activeSlice from "./slice/activeSlice";
 import footMenuSlice from "./slice/footMenuSlice";
 import dragSlice from "./slice/dragSlice";
+import authSlice from "./slice/authSlice";
 
 export const store = configureStore({
   reducer: {
@@ -10,5 +11,6 @@ export const store = configureStore({
     activing: activeSlice.reducer,
     footMenu: footMenuSlice.reducer,
     drag: dragSlice.reducer,
+    auth: authSlice.reducer,
   },
 });
