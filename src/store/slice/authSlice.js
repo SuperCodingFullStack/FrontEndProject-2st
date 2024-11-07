1e8667a7cbbfac84559efeb15e8f025e2f8cc594
hhaemin <ahrgoals@naver.com>
2024-11-07 10:23:05 +0900

    chore.로그인 상태 유지 구현

--- src/store/slice/authSlice.js
+++ src/store/slice/authSlice.js
@@ -3,17 +3,19 @@ import { createSlice } from "@reduxjs/toolkit";
 const authSlice = createSlice({
   name: "auth",
   initialState: {
-    isAuthenticated: false,
-    token: null,
+    isAuthenticated: !!localStorage.getItem("token"), // 토큰 유무에 따라 초기 상태 설정
+    token: localStorage.getItem("token"),
   },
   reducers: {
     login: (state, action) => {
       state.isAuthenticated = true;
       state.token = action.payload;
+      localStorage.setItem("token", action.payload); // 토큰을 localStorage에 저장
     },
     logout: (state) => {
       state.isAuthenticated = false;
       state.token = null;
+      localStorage.removeItem("token"); // 로그아웃 시 토큰 삭제
     },
   },
 });
